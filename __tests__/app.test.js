8dea97466676885a31ff72e45788063a9d9d66cd
Keyton Friske <keytonfriske@gmail.com>
2023-09-17 17:11:52 -0500

    Added jest tests

---
+++ __tests__/app.test.js
@@ -0,0 +1,135 @@
+const app = require('../src/app');
+const mock = jest.fn();
+
+describe("GET -- view grocery list", () => {
+    test('Grocery list starts as an empty array', async () => {
+        const result = app.getGroceryList();
+        
+        await expect(result).toHaveLength(0);
+    })   
+});
+
+describe("POST -- add new item to grocery list", () => {
+    test('Successfully adding an item to the grocery list should return true', () => {
+        const item = {name: "Test1", price: 1.00, quantity: 1, purchased: false};
+        const itemStatus = {onList: false, message: `${item.name} is not on the grocery list`};
+        const itemAdded = app.addItem(item, itemStatus);
+    
+        expect(itemAdded).toBeTruthy();
+    })
+
+    test('GET -- Grocery list contains added item', () => {
+        const groceryList = [{name: "Test1", price: 1.00, quantity: 1, purchased: false}];
+        const result = app.getGroceryList();
+    
+        expect(result).toEqual(groceryList);   
+    })
+
+    test('Adding an item without a name should return false', () => {
+        const item = {price: 1.50, quantity: 4, purchased: false};
+        const itemStatus = {onList: false, message: "Item does not have a valid name"};
+        const itemAdded = app.addItem(item, itemStatus);
+
+        expect(itemAdded).toBeFalsy();
+    })
+
+    test('Adding an item already on the grocery list should return false', () => {
+        const item = {name: "Test1", price: 1.00, quantity: 1, purchased: false};
+        const itemStatus = {onList: true, message: `${item.name} is already on the grocery list`};
+        const itemAdded = app.addItem(item, itemStatus);
+
+        expect(itemAdded).toBeFalsy();
+    })
+});
+
+describe("setItemStatus", () => {
+    test('Setting the status of an item without a name should set itemStatus.message to "Item does not have a valid name"', () => {
+        const item = {price: 1.00, quantity: 1, purchased: false};
+        const itemStatus = app.setItemStatus(item);
+    
+        expect(itemStatus.message).toBe("Item does not have a valid name");
+    })
+    
+    test("Setting the status of an item already on the grocery list should set onList to true and itemStatus.message to '${item.name} is already on the grocery list'", () => {
+        const item = {name: "Test1", price: 1.00, quantity: 1, purchased: false};
+        const itemStatus = app.setItemStatus(item);
+
+        expect(itemStatus.onList).toBeTruthy();
+        expect(itemStatus.message).toBe(`${item.name} is already on the grocery list`)
+    })
+
+    test("Setting the status of an item not on the grocery list should set onList to false and itemStatus.message to '${item.name} is not on the grocery list'", () => {
+        const item = {name: "Test2", price: 2.00, quantity: 2, purchased: false};
+        const itemStatus = app.setItemStatus(item);
+
+        expect(itemStatus.onList).toBeFalsy();
+        expect(itemStatus.message).toBe(`${item.name} is not on the grocery list`)
+    })
+
+})
+
+describe("PUT -- update item on grocery list", () => {
+    test('Successfully updating an item to the grocery list should return true', () => {
+        const item = {name: "Test1", purchased: true};
+        const itemStatus = {onList: true};
+        const itemUpdated = app.updateItem(item, itemStatus);
+    
+        expect(itemUpdated).toBeTruthy();
+    })
+
+    test('GET -- Grocery list contains updated item', () => {
+        const groceryList = [{name: "Test1", price: 1.00, quantity: 1, purchased: true}];
+        const result = app.getGroceryList();
+    
+        expect(result).toEqual(groceryList);   
+    })
+
+    test('Updating an item without a name should return false', () => {
+        const item = {price: 1.50, quantity: 4, purchased: false};
+        const itemStatus = {onList: false, message: "Item does not have a valid name"};
+        const itemUpdated = app.updateItem(item, itemStatus);
+
+        expect(itemUpdated).toBeFalsy();
+    })
+
+    test('Updating an item not on the grocery list should return false', () => {
+        const item = {name: "Test2", price: 2.00, quantity: 2, purchased: false};
+        const itemStatus = {onList: false, message: `${item.name} is not on the grocery list`};
+        const itemUpdated = app.updateItem(item, itemStatus);
+
+        expect(itemUpdated).toBeFalsy();
+    })
+});
+
+describe("DELETE -- delete item from grocery list", () => {
+    test('Successfully deleting an item from the grocery list should return true', () => {
+        const item = {name: "Test1"};
+        const itemStatus = {onList: true};
+        const itemDeleted = app.deleteItem(item, itemStatus);
+    
+        expect(itemDeleted).toBeTruthy();
+    })
+
+    test('GET -- Grocery list does not contain deleted item', () => {
+        const result = app.getGroceryList();
+    
+        expect(result).not.toHaveProperty('name', "Test1");
+    })
+
+    test('Deleting an item without a name should return false', () => {
+        const item = {price: 1.50, quantity: 4, purchased: false};
+        const itemStatus = {onList: false, message: "Item does not have a valid name"};
+        const itemDeleted = app.deleteItem(item, itemStatus);
+
+        expect(itemDeleted).toBeFalsy();
+    })
+
+    test('Deleting an item not on the grocery list should return false', () => {
+        const item = {name: "Test2", price: 2.00, quantity: 2, purchased: false};
+        const itemStatus = {onList: false, message: `${item.name} is not on the grocery list`};
+        const itemDeleted = app.deleteItem(item, itemStatus);
+
+        expect(itemDeleted).toBeFalsy();
+    })
+});
+
---
+++ src/app.js
@@ -0,0 +1,182 @@
+const http = require("http");
+const url = require('node:url');
+const PORT = 3000;
+const { createLogger, transports, format} = require('winston');
+module.exports = {getGroceryList, setItemStatus, addItem, deleteItem, updateItem};
+
+//Create Logger
+const logger = createLogger({
+    level: 'info',
+    format: format.combine(
+        format.timestamp(),
+        format.printf(({timestamp, level, message}) => {
+            return `${timestamp} [${level}]: ${message}`;
+        })
+    ),
+    transports: [
+        new transports.Console(),
+        new transports.File({ filename: 'app.log'}),
+    ]
+});
+
+
+//Initialize grocery list array
+const groceryList = [];
+
+//GET grocery list
+function getGroceryList(){
+    return groceryList;
+}
+
+//Set the status of an item
+function setItemStatus(item){
+    const itemStatus = {};
+    itemStatus.onList = false;
+    itemStatus.message = "";
+    
+    //Check if the item has a valid name
+    if(!item.name){
+        itemStatus.message = "Item does not have a valid name";
+    } else {
+    //Check if the item is already on the list
+        for(grocery of groceryList){
+            if(grocery.name === item.name){
+                itemStatus.onList = true;
+            }
+        }
+        if(itemStatus.onList){
+            itemStatus.message = `${item.name} is already on the grocery list`;
+        }else{
+            itemStatus.message = `${item.name} is not on the grocery list`;
+        }
+    }
+    return itemStatus;
+}
+
+//Add item to grocery list
+function addItem(item, itemStatus) {
+    if(item.name && !itemStatus.onList){
+        groceryList.push(item);
+        return true;
+    }else{
+        return false;
+    }
+}
+
+//Update an existing item on the grocery list
+function updateItem(itemUpdate, itemStatus){   
+    if(itemStatus.onList){
+        for (item of groceryList){
+            if(item.name === itemUpdate.name){
+                if("price" in itemUpdate){
+                    item.price = itemUpdate.price;
+                }else if("quantity" in itemUpdate){
+                    item.quantity = itemUpdate.quantity;
+                }else if("purchased" in itemUpdate){
+                    item.purchased = itemUpdate.purchased;
+                }    
+                return true;   
+            }
+        }
+    }else{
+        return false;
+    }
+}    
+    
+//Remove an item from the grocery list
+function deleteItem(item, itemStatus){
+    if(itemStatus.onList){
+        for(key in groceryList){
+            if(groceryList[key].name === item.name){
+                const removedItem = groceryList.splice(key, 1);
+                logger.info(`${removedItem[0].name} has been removed from the grocery list`);
+                return true;
+            }
+        }
+    }else{
+        return false;
+    }
+}
+
+//Create Server
+const server = http.createServer((req, res) => {
+        
+    //GET grocery list
+    if (req.method === 'GET' && req.url === '/grocerylist'){
+        res.writeHead(200, { 'Content-Type': 'application/json'});
+        logger.info(`User requested to see the grocery list`);
+        res.end(JSON.stringify(getGroceryList()));
+    
+    //POST item to grocery list
+    }else if(req.method === 'POST' && req.url === '/grocerylist/add'){
+        let body = '';
+        req.on('data', (chunk) => {
+            body += chunk;
+        });
+        req.on('end', () => {
+            const item = JSON.parse(body);
+            const itemStatus = setItemStatus(item);
+            
+            if(addItem(item, itemStatus)){
+                logger.info(`User added ${JSON.stringify(item)} to the grocery list`);
+                res.writeHead(201, {'Content-Type': 'application/json'});
+                res.end(JSON.stringify({message: `${(item.name)} Added Successfully!`}));
+            }else{
+                logger.error(`Invalid item attempted to be added to list. ${itemStatus.message}`);
+                res.writeHead(400, {'Content-Type': 'application/json'});
+                res.end(JSON.stringify({message: `${itemStatus.message}`}));
+            }
+        });  
+
+    //PUT update an item on the grocery list
+    }else if(req.method === "PUT" && req.url === `/grocerylist/update`){
+        let body = '';
+        req.on('data', (chunk) => {
+            body += chunk;
+        });
+        req.on('end', () => {
+            const item = JSON.parse(body);
+            const itemStatus = setItemStatus(item);
+
+            if(updateItem(item, itemStatus)){
+                logger.info(`User updated ${JSON.stringify(item)} on the grocery list`);
+                res.writeHead(201, {'Content-Type': 'application/json'});
+                res.end(JSON.stringify({message: `${(item.name)} Updated Successfully!`}));
+            }else{
+                logger.error(`Invalid item attempted to be updated on list. ${itemStatus.message}`);
+                res.writeHead(400, {'Content-Type': 'application/json'});
+                res.end(JSON.stringify({message: `${itemStatus.message}`}));
+            } 
+        });
+
+    //DELETE an item from the grocery list
+    }else if(req.method === "DELETE" && req.url === `/grocerylist/delete`){
+        let body = '';
+        req.on('data', (chunk) => {
+            body += chunk;
+        });
+        req.on('end', () => {
+            const item = JSON.parse(body);
+            const itemStatus = setItemStatus(item);
+
+            if(deleteItem(item, itemStatus)){
+                res.writeHead(201, {'Content-Type': 'application/json'});
+                res.end(JSON.stringify({message: `${item.name} Deleted Successfully!`}));
+            }else{
+                logger.error(`Invalid item attempted to be deleted from list. ${itemStatus.message}`);
+                res.writeHead(400, {'Content-Type': 'application/json'});
+                res.end(JSON.stringify({message: `${itemStatus.message}`}));
+            }
+        });
+
+    }else{
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.end('Not Found');
+    }
+})
+
+
+server.listen(PORT, () => {
+    console.log(`Server is listening on port http://localhost:${PORT}`);
+})
+
